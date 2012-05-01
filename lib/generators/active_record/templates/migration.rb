3c6b836dc04c3285d2df60796e3ff136b6b5589e
Carlos Vilhena <carlosvilhena@gmail.com>
2012-05-01 15:34:38 +0100

    change should be instance method

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,5 +1,5 @@
 class TwoFactorAuthenticationAddTo<%= table_name.camelize %> < ActiveRecord::Migration
-  def self.change
+  def change
     change_table :<%= table_name %> do |t|
       t.string   :second_factor_pass_code     , :limit => 32
       t.integer  :second_factor_attempts_count, :default => 0
