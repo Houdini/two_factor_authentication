811e91079d6e815bdceff9b3e0dba902a9a19622
Carlos Vilhena <carlosvilhena@gmail.com>
2012-05-01 13:45:41 +0100

    generate migration after adding :two_factor_authentication

--- lib/generators/two_factor_authentication/two_factor_authentication_generator.rb
+++ lib/generators/two_factor_authentication/two_factor_authentication_generator.rb
@@ -5,12 +5,13 @@ module TwoFactorAuthenticatable
 
       desc "Add :two_factor_authenticable directive in the given model. Also generate migration for ActiveRecord"
 
-      hook_for :orm
-
       def inject_two_factor_authentication_content
         path = File.join("app", "models", "#{file_path}.rb")
         inject_into_file(path, "two_factor_authenticatable, :", :after => "devise :") if File.exists?(path)
       end
+
+      hook_for :orm
+
     end
   end
 end
