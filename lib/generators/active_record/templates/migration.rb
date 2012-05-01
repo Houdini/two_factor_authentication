e2f54d9980aafc41270e4e33848c2063f9a93b98
Carlos Vilhena <carlosvilhena@gmail.com>
2012-05-01 13:41:06 +0100

    add migration template

---
+++ lib/generators/active_record/templates/migration.rb
@@ -0,0 +1,8 @@
+class TwoFactorAuthenticationAddTo<%= table_name.camelize %> < ActiveRecord::Migration
+  def self.change
+    change_table :<%= table_name %> do |t|
+      t.string   :second_factor_pass_code     , :limit => 32
+      t.integer  :second_factor_attempts_count, :default => 0
+    end
+  end
+end
