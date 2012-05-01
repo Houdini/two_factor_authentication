bf9f970cb60cd9592cc53bbd61683f374d57ffe2
Carlos Vilhena <carlosvilhena@gmail.com>
2012-05-01 13:50:20 +0100

    refactor the schema

--- lib/two_factor_authentication/schema.rb
+++ lib/two_factor_authentication/schema.rb
@@ -1,7 +1,10 @@
 module TwoFactorAuthentication
   module Schema
-    def two_factor_authenticatable
+    def second_factor_pass_code
       apply_devise_schema :second_factor_pass_code, String, :limit => 32
+    end
+
+    def second_factor_attempts_count
       apply_devise_schema :second_factor_attempts_count, Integer, :default => 0
     end
   end
