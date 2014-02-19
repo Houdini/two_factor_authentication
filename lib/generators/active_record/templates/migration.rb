98868bbece4887ab91c02ae0ee90431fae84f4c8
Matt Mueller <muellermr@gmail.com>
2014-02-19 14:34:59 -0600

    Change migration to add otp secret key.

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,7 +1,7 @@
 class TwoFactorAuthenticationAddTo<%= table_name.camelize %> < ActiveRecord::Migration
   def change
     change_table :<%= table_name %> do |t|
-      t.string   :second_factor_pass_code     , :limit => 32
+      t.string   :otp_secret_key
       t.integer  :second_factor_attempts_count, :default => 0
     end
   end
