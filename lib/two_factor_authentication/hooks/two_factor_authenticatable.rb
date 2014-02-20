d91d13fc1b6e02868e394036fb51d6e9023bd7a9
Matt Mueller <muellermr@gmail.com>
2014-02-20 09:29:18 -0600

    Remove code argument from send_two_factor_authentication_code

--- lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
+++ lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
@@ -1,8 +1,7 @@
 Warden::Manager.after_authentication do |user, auth, options|
   if user.respond_to?(:need_two_factor_authentication?)
     if auth.session(options[:scope])[:need_two_factor_authentication] = user.need_two_factor_authentication?(auth.request)
-      code = user.otp_code
-      user.send_two_factor_authentication_code(code)
+      user.send_two_factor_authentication_code
     end
   end
 end
