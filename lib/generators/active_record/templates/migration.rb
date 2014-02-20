ffab2c2dd2726a6326fde9a550d4c61174f9be61
Matt Mueller <muellermr@gmail.com>
2014-02-20 08:53:02 -0600

    Allow for successful rollback of migration.

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,8 +1,13 @@
 class TwoFactorAuthenticationAddTo<%= table_name.camelize %> < ActiveRecord::Migration
-  def change
+  def up
     change_table :<%= table_name %> do |t|
       t.string   :otp_secret_key
       t.integer  :second_factor_attempts_count, :default => 0
     end
+  end
+
+  def down
+    remove_column :<%= table_name %>, :otp_secret_key
+    remove_column :<%= table_name %>, :second_factor_attempts_count
   end
 end
