c87d5915411c3eaf1057c949b994d3313bd849cf
Laust Rud Jacobsen <laust@object.io>
2014-04-28 11:30:56 +0200

    Extract reused Warden key constant

--- lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
+++ lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
@@ -1,6 +1,6 @@
 Warden::Manager.after_authentication do |user, auth, options|
   if user.respond_to?(:need_two_factor_authentication?)
-    if auth.session(options[:scope])[:need_two_factor_authentication] = user.need_two_factor_authentication?(auth.request)
+    if auth.session(options[:scope])[TwoFactorAuthentication::NEED_AUTHENTICATION] = user.need_two_factor_authentication?(auth.request)
       user.send_two_factor_authentication_code
     end
   end
