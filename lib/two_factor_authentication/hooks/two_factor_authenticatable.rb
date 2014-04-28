20703c0397be2482d03d469a4b76f3a3aeaa621c
Laust Rud Jacobsen <laust@object.io>
2014-04-28 11:32:04 +0200

    Warden hook: refactor to not perform inline assignment for clarity

--- lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
+++ lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
@@ -1,6 +1,8 @@
 Warden::Manager.after_authentication do |user, auth, options|
   if user.respond_to?(:need_two_factor_authentication?)
-    if auth.session(options[:scope])[TwoFactorAuthentication::NEED_AUTHENTICATION] = user.need_two_factor_authentication?(auth.request)
+    need_code = user.need_two_factor_authentication?(auth.request)
+    auth.session(options[:scope])[TwoFactorAuthentication::NEED_AUTHENTICATION] = need_code
+    if need_code
       user.send_two_factor_authentication_code
     end
   end
