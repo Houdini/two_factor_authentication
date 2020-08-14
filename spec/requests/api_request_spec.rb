34689706b7ce70e0856996ecc0fceb832ca42b06
Jarkko Laine <jarkko@jlaine.net>
2020-08-14 10:42:35 +0300

    Make the spec a request spec

---
+++ spec/requests/api_request_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe "API request", type: :request do
+  context "when logged in" do
+    let(:user) { create_user("encrypted", email: "foo@example.com", otp_secret_key: "6iispf5cjufa4vsm") }
+
+    before do
+      sign_in user
+    end
+
+    context "with json" do
+      it "returns 401 when path requires authentication" do
+        get "/dashboard.json"
+        expect(response.response_code).to eq(401)
+        expect(JSON.parse(response.body)["redirect_to"]).to eq(user_two_factor_authentication_path)
+      end
+
+      context "after TFA" do
+        it "returns successfully" do
+          get "/dashboard.json"
+          expect(response.response_code).to eq(401)
+          expect(JSON.parse(response.body)["redirect_to"]).to eq(user_two_factor_authentication_path)
+          totp_code = ROTP::TOTP.new(user.otp_secret_key, digits: 6).at(Time.now)
+          put "/users/two_factor_authentication", params: { code: totp_code }
+          get "/dashboard.json"
+          expect(response.response_code).to eq(200)
+          body = JSON.parse(response.body)
+          expect(body["success"]).to eq(true)
+        end
+      end
+    end
+
+    context "with xml" do
+      it "returns 401 when path requires authentication" do
+        get "/dashboard.xml"
+        expect(response.response_code).to eq(401)
+      end
+
+      context "after TFA" do
+        it "returns successfully" do
+          totp_code = ROTP::TOTP.new(user.otp_secret_key, digits: 6).at(Time.now)
+          put "/users/two_factor_authentication", params: { code: totp_code }
+          get "/dashboard.xml"
+          expect(response.response_code).to eq(200)
+          expect(response.body).to eq("<success></success>")
+        end
+      end
+    end
+  end
+end
