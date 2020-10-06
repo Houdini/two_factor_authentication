56d45f84ca575706b5df6c4dc25d4bb8e0de8143
Jarkko Laine <jarkko@jlaine.net>
2020-10-06 16:07:53 +0300

    Correct key

--- lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
+++ lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
@@ -1,7 +1,6 @@
 Warden::Manager.after_authentication do |user, auth, options|
-  debugger
   scopes_to_skip = [Rails.application.config.devise.skip_send_new_otp_in_after_set_user_for].compact.flatten
-  unless options["scope"].in?(scopes_to_skip)
+  unless options[:scope].in?(scopes_to_skip)
     if auth.env["action_dispatch.cookies"]
       expected_cookie_value = "#{user.class}-#{user.public_send(Devise.second_factor_resource_id)}"
       actual_cookie_value = auth.env["action_dispatch.cookies"].signed[TwoFactorAuthentication::REMEMBER_TFA_COOKIE_NAME]
