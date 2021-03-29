30bce037c281f7590f3a22c126319f4a9440e6f0
Greg Myers <neonmd@hotmail.co.uk>
2021-03-29 15:23:20 +0100

    Remigrate the DB

--- spec/rails_app/db/migrate/20161110120108_add_enable_otp_to_user.rb
+++ spec/rails_app/db/migrate/20161110120108_add_enable_otp_to_user.rb
@@ -1,4 +1,4 @@
-class AddEnableOtpToUser < ActiveRecord::Migration
+class AddEnableOtpToUser < ActiveRecord::Migration[4.2]
   def change
     add_column :users, :otp_enabled, :boolean, default: false
   end
--- spec/rails_app/db/schema.rb
+++ spec/rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2016_02_09_032439) do
+ActiveRecord::Schema.define(version: 2016_11_10_120108) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
