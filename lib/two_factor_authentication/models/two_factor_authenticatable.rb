1cc85708167a84ad04193f92cc51a5c96a8ae6c3
Kalpesh Fulpagare <kalpesh.fulpagare@gmail.com>
2022-04-14 11:18:58 +0530

    Rails 6 changes

--- lib/two_factor_authentication/models/two_factor_authenticatable.rb
+++ lib/two_factor_authentication/models/two_factor_authenticatable.rb
@@ -101,7 +101,7 @@ module Devise
         def create_direct_otp(options = {})
           # Create a new random OTP and store it in the database
           digits = options[:length] || self.class.direct_otp_length || 6
-          update_attributes(
+          update(
             direct_otp: random_base10(digits),
             direct_otp_sent_at: Time.now.utc
           )
@@ -122,7 +122,7 @@ module Devise
         end
 
         def clear_direct_otp
-          update_attributes(direct_otp: nil, direct_otp_sent_at: nil)
+          update(direct_otp: nil, direct_otp_sent_at: nil)
         end
       end
 
